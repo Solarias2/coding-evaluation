7f447a9c53a34080bb19a5901a18d6d1f69b8b49
Solarias2 <82850976+Solarias2@users.noreply.github.com>
2024-07-18 14:12:23 -0700

    Update Organization.java

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -20,8 +20,48 @@ public abstract class Organization {
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
+        //Modified to call a recursive function.
+        return hireRecursive(root, person, title);
+    }
+
+    /*
+     * Recursive function to tranverse through the tree in order 
+     * to find the available position and fill it out based off the
+     * attributes from the original hire method.
+     */
+    private Optional<Position> hireRecursive(Position current, Name person, String title) {
+        /*
+         * If the title exists in the Position class, it would 
+         * assign the role to the person associated in the tree.
+         */
+        if (current.getTitle().equals(title)) {
+            current.setEmployee(Optional.of(new Employee(generateIdentifier(), person)));
+            return Optional.of(current);
+        }
+
+        /*
+         * Once assigned, the for loop runs and tranverses down 
+         * the tree. When the employee is hired from 
+         * the previous code, it would appear in the for loop which would
+         * fulfill the if condition, stopping the recursion and overall method,
+         * saving the instance. 
+         */
+        for (Position directReport : current.getDirectReports()) {
+            Optional<Position> hiredPosition = hireRecursive(directReport, person, title);
+            if (hiredPosition.isPresent()) {
+                return hiredPosition;
+            }
+        }
+        //Returns if nothing is assigned or 
         return Optional.empty();
+    }
+
+    /*
+     * Method to randomly select a number to assign an identifier for the employee.
+     * Simple as it is only a coding interview and not apart of a main architecture.
+     */
+    private int generateIdentifier() {
+        return (int) (Math.random() * 10000);
     }
 
     @Override
